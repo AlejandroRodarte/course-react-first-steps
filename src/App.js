3010ed6f8d23c7b65961e6ea135a9b3af8c32e9a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-02 13:21:05 -0600

    Handling Events with Methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Person from './Person/Person';
@@ -23,13 +22,17 @@ class App extends Component {
     ]
   };
 
+  switchNameHandler = () => {
+    console.log('clicked!');
+  };
+
   render() {
 
     return (
 
       <div className="App">
 
-        <button>
+        <button onClick={ this.switchNameHandler }>
           Switch name
         </button>
 
