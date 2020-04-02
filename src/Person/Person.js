944d522c9b620203bec22dba1ac9a69d8d5bd950
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-02 13:08:57 -0600

    Understanding the "children" Prop

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Person = ({ name, age }) => (
-    <p>
-        I'm { name } and I am { age } years old!
-    </p>
+const Person = ({ name, age, children }) => (
+    <div>
+        <p>
+            I'm { name } and I am { age } years old!
+        </p>
+        <p>
+            { children }
+        </p>
+    </div>
 );
 
 export default Person;
