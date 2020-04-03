370599ac87ed2a6a12b61f363b3ec7004e341d2f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-02 22:42:25 -0600

    CSS Modules and Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import './Person.css';
-
-// styled-component generates components
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-        color: red;
-    }
-`;
+import classes from './Person.module.css';
 
 const Person = ({ name, age, children, click, changed }) => {
 
     return (
-        <StyledDiv>
+        <div className={ classes.Person }>
 
             <p onClick={ click }>
                 I'm { name } and I am { age } years old!
@@ -37,7 +21,7 @@ const Person = ({ name, age, children, click, changed }) => {
                 value={ name }
             />
     
-        </StyledDiv>
+        </div>
     );
 
 }
